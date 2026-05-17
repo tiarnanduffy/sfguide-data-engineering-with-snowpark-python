47610cc93d96a7126b9bed64685657b42c580c67
tiarnanduffy <tiarnanduffy7@gmail.com>
2026-05-17 17:04:07 +0100

    use scipy for udf

--- steps/05_fahrenheit_to_celsius_udf/fahrenheit_to_celsius_udf/function.py
+++ steps/05_fahrenheit_to_celsius_udf/fahrenheit_to_celsius_udf/function.py
@@ -10,10 +10,10 @@
 # SNOWFLAKE ADVANTAGE: SnowCLI (PuPr)
 
 import sys
+from scipy.constants import convert_temperature
 
 def main(temp_f: float) -> float:
-    return (float(temp_f) - 32) * (5/9)
-
+    return convert_temperature(float(temp_f), 'F', 'C')
 
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
@@ -22,3 +22,4 @@ if __name__ == '__main__':
         print(main(*sys.argv[1:]))  # type: ignore
     else:
         print(main())  # type: ignore
+
